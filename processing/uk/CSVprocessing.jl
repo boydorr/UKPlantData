c3fae1c4bedfdf754257dd270c4217ec9751025d
Claire Harris <claire.harris@bioss.ac.uk>
2023-08-31 23:20:43 +0100

    Update unzip for linux

--- processing/uk/CSVprocessing.jl
+++ processing/uk/CSVprocessing.jl
@@ -7,7 +7,11 @@ using ExcelFiles
 handle = DataPipeline.initialise()
 path = link_read!(handle, "UKModel/PLANTATT")
 xlspath = joinpath(splitpath(path)[1:end-1])
-run(`tar xvf $path -C $xlspath`)
+if Sys.iswindows()
+    run(`tar xvf $path -C $xlspath`)
+else
+    run(`unzip $path`)
+end
 xlspath = joinpath(xlspath, "PLANTATT_19_Nov_08.xls")
 data = DataFrame(ExcelFiles.load(xlspath, "Data"))
 newpath = link_write!(handle, "PLANTATT-data")
@@ -16,7 +20,11 @@ CSV.write(newpath, data)
 # BRYOATT
 path = link_read!(handle, "UKModel/BRYOATT")
 xlspath = joinpath(splitpath(path)[1:end-1])
-run(`tar xvf $path -C $xlspath`)
+if Sys.iswindows()
+    run(`tar xvf $path -C $xlspath`)
+else
+    run(`unzip $path`)
+end
 xlspath = joinpath(xlspath, "Bryoatt_updated_2017/Bryoatt_updated_2017.xls")
 data = DataFrame(ExcelFiles.load(xlspath, "BRYOATT"))
 newpath = link_write!(handle, "BRYOATT-data")
@@ -25,7 +33,11 @@ CSV.write(newpath, data)
 # GBIF
 path = link_read!(handle, "UKModel/GBIF-raw")
 zippath = joinpath(splitpath(path)[1:end-1])
-run(`tar xvf $path -C $zippath`)
+if Sys.iswindows()
+    run(`tar xvf $path -C $zippath`)
+else
+    run(`unzip $path`)
+end
 data = CSV.read(joinpath(zippath, "0025546-190918142434337.csv"), DataFrame, normalizenames=true)
 newpath = link_write!(handle, "GBIF-records")
 CSV.write(newpath, data)
