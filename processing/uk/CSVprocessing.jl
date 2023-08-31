2d74c0ccc152c467070420e52fac05db9c39aa6b
Claire Harris <claire.harris@bioss.ac.uk>
2023-08-31 11:07:48 +0100

    Yaml files for running pipeline and some code for processing csv zip files.

---
+++ processing/uk/CSVprocessing.jl
@@ -0,0 +1,35 @@
+using CSV
+using DataPipeline
+using DataFrames
+using ExcelFiles
+
+# PLANTATT
+handle = DataPipeline.initialise()
+path = link_read!(handle, "UKModel/PLANTATT")
+xlspath = joinpath(splitpath(path)[1:end-1])
+run(`tar xvf $path -C $xlspath`)
+xlspath = joinpath(xlspath, "PLANTATT_19_Nov_08.xls")
+data = DataFrame(ExcelFiles.load(xlspath, "Data"))
+newpath = link_write!(handle, "PLANTATT-data")
+CSV.write(newpath, data)
+
+# BRYOATT
+path = link_read!(handle, "UKModel/BRYOATT")
+xlspath = joinpath(splitpath(path)[1:end-1])
+run(`tar xvf $path -C $xlspath`)
+xlspath = joinpath(xlspath, "Bryoatt_updated_2017/Bryoatt_updated_2017.xls")
+data = DataFrame(ExcelFiles.load(xlspath, "BRYOATT"))
+newpath = link_write!(handle, "BRYOATT-data")
+CSV.write(newpath, data)
+
+# GBIF
+path = link_read!(handle, "UKModel/GBIF-raw")
+zippath = joinpath(splitpath(path)[1:end-1])
+run(`tar xvf $path -C $zippath`)
+data = CSV.read(joinpath(zippath, "0025546-190918142434337.csv"), DataFrame, normalizenames=true)
+newpath = link_write!(handle, "GBIF-records")
+CSV.write(newpath, data)
+
+DataPipeline.finalise(handle)
+
+
